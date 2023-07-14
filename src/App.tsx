548de4b8bff5d90d590b6a2494b7e1082ebcc69c
Opdata <pb03641@gmail.com>
2023-07-14 17:15:29 +0900

    feat: API get 기초코드

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,33 @@ import './App.css';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 function App() {
+  const { isLoading, isFetching, data, error } = useQuery({
+    queryKey: ['getPost'],
+    queryFn: () => {
+      return fetch('https://jsonplaceholder.typicode.com/posts/1').then((res) =>
+        res.json()
+      );
+    },
+  });
   const queryClient = useQueryClient();
 
-  const postHandler = async () => {};
-  const putHandler = async () => {};
+  if (isLoading) {
+    <p>isLoading...</p>;
+  }
+
+  if (!error) {
+    <p>is Error</p>;
+  }
 
   return (
     <>
-      <div></div>
+      <div>
+        <p>{data?.userId}</p>
+        <p>{data?.id}</p>
+        <p>{data?.title}</p>
+        <p>{data?.body}</p>
+      </div>
+      {isFetching && <p>isFetching...</p>}
       <button onClick={postHandler}>post api</button>
       <button onClick={putHandler}>put api</button>
     </>
